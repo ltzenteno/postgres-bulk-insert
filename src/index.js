6beb0175563ad74e58b4de62a21633863ad01a55
Luis Zenteno <ltzenteno@gmail.com>
2018-09-07 13:15:03 -0500

    makes email unique

--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ const length = 50000;
 
 const createUser = id => {
   const name = createRandomFirstName();
-  return `${id},${name},${createRandomFirstSurname()},${createRandomSecondSurname()},${createEmail(name)},${createPassword()},1,1,t,t,f\n`;
+  return `${id},${name},${createRandomFirstSurname()},${createRandomSecondSurname()},${createEmail(name, id)},${createPassword()},1,1,t,t,f\n`;
 };
 
 const createPerson = id => {
   const name = createRandomFirstName();
-  return `${id},${name},${createRandomFirstSurname()},${createRandomSecondSurname()},30,${id},1,1,${createEmail(name)},,1,INT,2018-08-28 15:21:04.239836,${id},t,f,{} \n`;
+  return `${id},${name},${createRandomFirstSurname()},${createRandomSecondSurname()},30,${id},1,1,${createEmail(name, id)},,1,INT,2018-08-28 15:21:04.239836,${id},t,f,{} \n`;
 };
 
 const createUsersFile = () => {
@@ -25,7 +25,7 @@ const createUsersFile = () => {
   stream.once('open', fd => {
     stream.write('id,nombre,apellido_paterno,apellido_materno,email,password,idioma_id,rol_id,estatus,primer_visita,eliminado \n');
     for(let i=0;i<length;i++){
-      stream.write(createUser(i+10));
+      stream.write(createUser(i+200));
     }
     stream.end();
   });
@@ -36,7 +36,7 @@ const createPersonsFile = () => {
   stream.once('open', fd => {
     stream.write('id,nombre,apellido_paterno,apellido_materno,edad,usuario_id,sucursal_id,tipo_persona_id,email,foto_perfil,perfil_base_id,prefijo,fecha_registro,digito,estatus,eliminado,adicionales \n');
     for(let i=0;i<length;i++){
-      stream.write(createPerson(i+10));
+      stream.write(createPerson(i+200));
     }
     stream.end();
   });
--- src/util.js
+++ src/util.js
@@ -10,7 +10,7 @@ const createRandomSecondSurname = () => {
   return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 8);
 };
 
-const createEmail = name => `${name}@mail.com`;
+const createEmail = (name, id) => `${name}${id}@mail.com`;
 
 const createPassword = () => {
   return 'ccee5504c9d889922b101124e9e43b71';
